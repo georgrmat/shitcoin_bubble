b0f7acc8416af7a0041e0da79939b376dfea976e
Georges <92797658+georgrmat@users.noreply.github.com>
2023-12-26 11:45:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 df_pairs = pd.read_csv("pairs.csv")
-st.write(df)
+st.write(df_pairs)
 
 exchange = st.sidebar.radio("Select an exchange", df_pairs["exchange"].unique())
 base_asset = st.sidebar.radio("Base Asset", ["BTC", "ETH"])
