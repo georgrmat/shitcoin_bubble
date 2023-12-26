fb7b58d415d9404397d722eb92c07200364b5927
Georges <92797658+georgrmat@users.noreply.github.com>
2023-12-26 11:46:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-df_pairs = pd.read_csv("pairs.csv")
+df_pairs = pd.read_csv("pairs.csv", sep = ";")
 st.write(df_pairs)
 
 exchange = st.sidebar.radio("Select an exchange", df_pairs["exchange"].unique())
